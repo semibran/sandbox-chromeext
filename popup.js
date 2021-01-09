79f8ecb098dab3858b31b4b2c98f5f7a95e58ad3
Brandon Semilla <semibran@gmail.com>
2021-01-08 21:05:55 -0800

    click to play

--- popup.js
+++ popup.js
@@ -1,12 +1,28 @@
 const secs = 3
 const context = new AudioContext()
 const bufferSize = context.sampleRate * secs
-const buffer = context.createBuffer(2, 22050, context.sampleRate)
+const buffer = context.createBuffer(1, 22050, context.sampleRate)
+
+let data = buffer.getChannelData(0)
+for (let i = 0; i < bufferSize; i++) {
+  data[i] = Math.random() * 2 - 1
+}
+
+let noise = context.createBufferSource()
+noise.buffer = buffer
+noise.connect(context.destination)
 
 let playing = false
-const toggle = document.getElementById('toggle')
-if (toggle) {
-  toggle.onclick = _ => {
-    playing = !playing
+const button = document.getElementById('toggle')
+if (button) {
+  button.onclick = toggle
+}
+
+function toggle () {
+  playing = !playing
+  if (playing) {
+    noise.start()
+  } else {
+    noise.stop()
   }
 }
